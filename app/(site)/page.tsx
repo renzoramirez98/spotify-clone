cdde9e4121ef63dd5496f34247d38f7e0e6b4b4f
Renzo <renzoramirezsalvatierra@gmail.com>
2024-11-07 21:02:20 +1100

    for pull request

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="text=green-500">
+      Hello (Site)!
+    </div>
+  )
+}
+
+// checking if this commit message will go
+
+// now in main to check for renzo branch to pull this into my branch within vs code
